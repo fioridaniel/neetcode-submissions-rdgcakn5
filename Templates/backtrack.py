e038776ba9d07e5b39545334c179caa6afde3315
fiori_daniel <daniel.fiori.souza@uel.br>
2026-04-13 09:50:33 -0300

    feat: add templates folder

---
+++ Templates/backtrack.py
@@ -0,0 +1,12 @@
+def backtrack(params):
+    if base_case_condition:
+        save_result
+        return
+
+    for choice in choices:
+        if violates_constraints:
+            continue
+
+        make_choice
+        backtrack(updated_params)
+        undo_choice  # Backtracking Step
