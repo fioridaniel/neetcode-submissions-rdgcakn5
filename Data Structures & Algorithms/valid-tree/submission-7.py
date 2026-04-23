cb6761b6db1fe1fc92da96c095d9a221a9017b0c
fioridaniel <163748937+fioridaniel@users.noreply.github.com>
2026-04-23 08:04:12 -0300

    Add: valid-tree - submission-7

---
+++ Data Structures & Algorithms/valid-tree/submission-7.py
@@ -0,0 +1,30 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        edgesMap = {}
+
+        for i in range(n):
+            edgesMap[i] = []    
+        
+        for i in range(len(edges)):
+            a, b = edges[i]
+            edgesMap[a].append(b)
+            edgesMap[b].append(a)
+
+        visited = set()
+        
+        def dfs(node, parent):
+            if node in visited:
+                return False
+            
+            visited.add(node)
+
+            for nei in edgesMap[node]:
+                if nei == parent:
+                    continue
+                if not dfs(nei, node):
+                    return False
+
+            return True
+
+        return dfs(0, -1) and len(visited) == n
+            
